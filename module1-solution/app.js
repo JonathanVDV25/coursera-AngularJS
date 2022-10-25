391e49ef4fdb00491c3943c813478f656e7eb738
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-10-25 23:53:56 +0200

    Enhance current code

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,16 +9,20 @@ function LunchCheckController($scope) {
 
   $scope.dishesString = '';
   $scope.result = '';
+  $scope.state = 'white';
 
   $scope.checkTooMuch = function () {
-    console.log($scope.dishesString.length);
-    if ($scope.dishesString.length === 0) {
+    if ($scope.dishesString.length === 0 || $scope.dishesString === '""' ||
+        $scope.dishesString === "''") {
       $scope.result = "Please enter data first";
+
+      $scope.state = 'NOK';
       return;
     }
 
     var nbrDishes = $scope.dishesString.split(",").length;
     nbrDishes <= 3 ? $scope.result = "Enjoy!" : $scope.result = "Too Much";
+    $scope.state = 'OK';
   };
 
 }
